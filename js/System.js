7d4c7cab237c8b3fd6efee4be9166ad3bcf10f8c
missbhargava <78480343+missbhargava@users.noreply.github.com>
2021-06-28 17:11:40 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,37 @@
+class System{
+
+    constructor(){
+
+    }
+
+    // Add code to authenticate the given code and the access codes.
+    authenticate(accessCode1,enteredCode){
+        if(accessCode1===enteredCode){
+            return true
+        }else{
+          return false
+
+        }       
+    }
+
+    authenticate(accessCode2,enteredCode){
+        if(accessCode2===enteredCode){
+            return true
+        }else{
+          return false
+
+        }       
+    }
+
+    authenticate(accessCode2,enteredCode){
+        if(accessCode2===enteredCode){
+            return true
+        }else{
+          return false
+
+        }       
+    }
+
+    
+
+}
